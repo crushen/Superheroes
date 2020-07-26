af27669b55aff7b97a85c43cb945f2440f743db4
Charlotte Rushen <charlotterushen@hotmail.com>
2020-07-26 19:53:31 +0100

    Home page done

---
+++ src/components/cards/TitleCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <p>{{ card.title }}</p>
+    <img
+      :style="{ width: `${card.background.width}px` }"
+      :src="card.background.src"
+      alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    card: { required: true, type: Object }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
+
+.card {
+  height: 100px;
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  border-radius: $border-radius;
+  position: relative;
+  overflow: hidden;
+
+  p {
+    font-family: 'Bangers', cursive;
+    font-size: 32px;
+    letter-spacing: 1px;
+    color: $dark-font;
+    position: relative;
+    z-index: 5;
+  }
+
+  img {
+    position: absolute;
+    z-index: 0;
+    right: -60px;
+    transform: rotate(-25deg);
+    opacity: 0.75;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,33 @@
 <template>
-  <section class="home">
+  <div>
+  <!-- <section class="home">
     <div v-if="loading">
       <p>... loading</p>
     </div>
 
     <div v-else>
+      <div 
+        :style="{backgroundImage: `url(${featuredSuperhero.images.md})`}"
+        class="card">
+        <div class="overlay"></div>
+        <div class="body">
+          <div class="name">
+            <p>{{ featuredSuperhero.name }}</p>
+          </div>
+        </div>
+      </div>
+
       <input 
         v-model="search"
         type="text" 
         placeholder="Search superheroes">
 
-      <ul>
+      <ul v-if="search">
         <li 
           v-for="hero in filteredSuperheroes"
           :key="hero.id">
           <p>{{ hero.name }}</p>
-          <img :src="hero.images.sm" @alt="`An image of ${hero.name}`">
+          <img :src="hero.images.sm" :alt="`An image of ${hero.name}`">
         </li>
       </ul>
     </div>
@@ -23,16 +35,52 @@
     <div v-if="error">
       <p>{{ error }}</p>
     </div>
+  </section> -->
+
+  <header class="content padding-top">
+    <h1 class="heading one">Superhero <span>Database</span></h1>
+    <h2 class="heading four">All superheroes from all <br>of the universes in one place</h2>
+  </header>
+
+  <section class="content">
+    <div id="hero-search">
+      <label for="search">Search for your favorite hero to <br>find out their stats and background</label>
+      <div class="input">
+        <img src="@/assets/icons/search.svg" alt="">
+        <input id="search" type="search" autocomplete="off">
+      </div>
+    </div>
   </section>
+
+  <section class="cards content padding-bottom">
+    <title-card
+      v-for="card in titleCards"
+      :key="card.title"
+      :card="card"
+      class="title-card" />
+  </section>
+  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import titleCard from '@/components/cards/TitleCard'
 
 export default {
+  components: {
+    titleCard
+  },
   data() {
     return {
-      search: null
+      search: null,
+      titleCards: [ 
+        { title: "Marvel", background: { src: require('@/assets/logos/marvel.svg'), width: 240 } },
+        { title: "DC Comics", background: { src: require('@/assets/logos/dc.svg'), width: 240 } },
+        { title: "Dark Horse", background: { src: require('@/assets/logos/dark-horse.svg'), width: 220 } },
+        { title: "Star Wars", background: { src: require('@/assets/logos/star-wars-2.svg'), width: 220 } },
+        { title: "Star Trek", background: { src: require('@/assets/logos/star-trek.svg'), width: 200 } },
+        { title: "Image Comics", background: { src: require('@/assets/logos/image.svg'), width: 200 } }
+      ]
     }
   },
   computed: {
@@ -47,6 +95,9 @@ export default {
       }
 
       return results
+    },
+    featuredSuperhero() {
+      return this.superheroes[Math.floor(Math.random() * this.superheroes.length)];
     }
   },
   mounted() {
@@ -54,3 +105,108 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
+
+header {
+  text-align: center;
+}
+
+h1 {
+  span {
+    display: block;
+    //margin-left: 40px;
+  }
+}
+
+h2 {
+  margin-top: 24px;
+}
+
+#hero-search {
+  margin-top: 74px;
+
+  label {
+    width: 100%;
+    text-align: center;
+  }
+
+  .input {
+    position: relative;
+    margin-top: 16px;
+  }
+
+  img {
+    width: 24px;
+    position: absolute;
+    top: 6px;
+    left: 6px;
+  }
+
+  input {
+    padding: 10px 16px 10px 40px;
+    box-sizing: border-box;
+  }
+}
+
+.cards {
+  margin-top: 48px;
+
+  .title-card {
+    background: $body-med;
+
+    &:nth-of-type(even) {
+      background: $body-light;
+    }
+
+    &:not(:first-of-type) {
+      margin-top: 16px;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+// .home {
+//   width: 90%;
+//   margin: auto;
+// }
+// .card {
+//   min-height: 400px;
+//   position: relative;
+//   border-radius: 16px;
+//   background-position: center;
+
+//   .overlay {
+//     width: 100%;
+//     height: 100%;
+//     position: absolute;
+//     z-index: 0;
+//     background: rgba($color: #031126, $alpha: 0.6);
+//     border-radius: 16px;
+//   }
+
+//   .body {
+//     min-height: 400px;
+//     display: flex;
+//     align-items: flex-end;
+//     padding: 16px;
+//     position: relative;
+//     z-index: 5;
+
+//     .name {
+//       font-size: 50px;
+//       margin: 0;
+//       letter-spacing: 2px;
+//       color: white;
+//       font-family: 'Bangers', cursive;
+//     }
+//   }
+// }
+</style>
