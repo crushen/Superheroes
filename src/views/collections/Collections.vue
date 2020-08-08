dac7b7117b104edf3c48eb0ed1d16521883f0d01
Charlotte Rushen <charlotterushen@hotmail.com>
2020-08-08 17:39:27 +0100

    Added slugify method

--- src/views/collections/Collections.vue
+++ src/views/collections/Collections.vue
@@ -71,6 +71,22 @@ export default {
       return sorted
     }
   },
+  methods: {
+    slugify(string) {
+    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+    const p = new RegExp(a.split('').join('|'), 'g')
+
+    return string.toString().toLowerCase()
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+      .replace(/&/g, '-and-') // Replace & with 'and'
+      .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+      .replace(/\-\-+/g, '-') // Replace multiple - with single -
+      .replace(/^-+/, '') // Trim - from start of text
+      .replace(/-+$/, '') // Trim - from end of text
+    }
+  },
   mounted() {
     this.$store.dispatch('loadSuperheroes')
   }
