d15158dfb4de60ac888288a3935fbba96215f2ee
Charlotte Rushen <charlotterushen@hotmail.com>
2020-08-08 13:56:42 +0100

    Nav animated

--- src/components/nav/Nav.vue
+++ src/components/nav/Nav.vue
@@ -1,28 +1,26 @@
 <template>
   <header>
-    <div class="nav-bar-container">
-      <div class="nav-bar">
-        <button
-          v-if="innerWidth < 1000"
-          @click="openNav"
-          class="nav-button">
-          <div class="nav-line"></div>
-          <div class="nav-line"></div>
-          <div class="nav-line"></div>
-        </button>
-        <!-- <div v-else>
-          <nav>
-            <nuxt-link to="/" exact>Home</nuxt-link>
-            <nuxt-link to="/work">Work</nuxt-link>
-            <nuxt-link to="/contact">Contact</nuxt-link>
-          </nav>
-        </div> -->
-      </div>
+    <div class="nav-bar">
+      <button
+        v-if="innerWidth < 1000"
+        @click="openNav"
+        class="nav-button">
+        <div class="nav-line"></div>
+        <div class="nav-line"></div>
+        <div class="nav-line"></div>
+      </button>
+      <!-- <div v-else>
+        <nav>
+          <nuxt-link to="/" exact>Home</nuxt-link>
+          <nuxt-link to="/work">Work</nuxt-link>
+          <nuxt-link to="/contact">Contact</nuxt-link>
+        </nav>
+      </div> -->
     </div>
 
     <transition 
-      enter-active-class="animated slideInRight faster" 
-      leave-active-class="animated slideOutRight faster">
+      enter-active-class="animate__animated animate__slideInLeft" 
+      leave-active-class="animate__animated animate__slideOutLeft">
       <div 
         v-if="active" 
         v-on-clickaway="closeNav"
@@ -31,16 +29,14 @@
           <router-link 
             :to="{ name: 'Home' }" 
             exact 
-            @click.native="closeNav"
-            class="animated fadeInRight">
+            @click.native="closeNav">
             Home
           </router-link>
 
           <router-link 
             :to="{ name: 'Collections' }" 
             exact 
-            @click.native="closeNav"
-            class="animated fadeInRight">
+            @click.native="closeNav">
             Collections
           </router-link>
         </nav>
@@ -63,9 +59,13 @@ export default {
   methods: {
     openNav() {
       this.active = true
+      document.body.scrollTop = 0
+      document.documentElement.scrollTop = 0
+      document.querySelector('body').style.overflow = 'hidden'
     },
     closeNav() {
       this.active = false
+      document.querySelector('body').style.overflow = 'auto'
     }
   },
   mounted() {
@@ -79,22 +79,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
 
-.nav-bar-container {
-  position: fixed;
-  width: 100vw;
-  padding: 16px 7.5%;
-  background: transparent;
-  z-index: 10;
-  transition: 0.6s;
+.animate__animated {
+  &.animate__slideInLeft,
+  &.animate__slideOutLeft {
+    --animate-duration: 0.4s;
+  }
 }
 
 .nav-bar {
+  width: 100%;
+  position: absolute;
+  padding: 24px 7.5vw;
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  max-width: 1300px;
-  margin: auto;
+  justify-content: flex-end;
 }
 
 .nav-button {
@@ -115,73 +115,56 @@ export default {
   margin-bottom: 4px;
 }
 
-a:hover {
-  text-decoration: none;
-}
-
 /* RIGHT NAV DRAWER */
 
 .nav-container {
-  height: 100vh;
-  width: 75%;
-  max-width: 400px;
-  background: #484f6b;
+  height: calc(100vh - 220px);
+  width: 100%;
+  background: $accent;
   position: fixed;
-  top: 0;
+  top: 220px;
   right: 0;
   z-index: 20;
 }
 
 nav {
+  margin-top: 25%;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  margin: 100px 0 0 32px;
+  align-items: center;
 }
 
 nav a {
-  font-size: 20px;
-  line-height: 50px;
-  font-weight: 700;
+  font-size: 18px;
+  font-weight: 600;
   animation-duration: 0.4s;
   animation-timing-function: ease-out;
+  text-decoration: none;
+  color: $light-font;
+  opacity: 0.7;
+
+  &:not(:first-of-type) {
+    margin-top: 16px;
+  }
 }
 
-nav a:nth-of-type(1) {
-  animation-delay: 0.2s;
-}
-
-nav a:nth-of-type(2) {
-  animation-delay: 0.3s;
-}
-
-nav a:nth-of-type(3) {
-  animation-delay: 0.4s;
-}
-
-nav .router-link-active:nth-of-type(1) {
-  color: #ffb2ab;
-}
-nav .router-link-active:nth-of-type(2) {
-  color: #e0bce6;
-}
-nav .router-link-active:nth-of-type(3) {
-  color: #a9e3eb;
+nav .router-link-active {
+  opacity: 1;
 }
 
 /* TABLET + */
 
-@media screen and (min-width: 1000px){
-  nav {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin: 0;
-  }
+// @media screen and (min-width: 1000px){
+//   nav {
+//     display: flex;
+//     flex-direction: row;
+//     align-items: center;
+//     margin: 0;
+//   }
 
-  nav a {
-    margin-left: 32px;
-    font-size: 16px;
-  }
-}
+//   nav a {
+//     margin-left: 32px;
+//     font-size: 16px;
+//   }
+// }
 </style>
